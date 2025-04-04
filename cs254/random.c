34057da2cc0a0bb150e28a7689a28055a07a8ab9
aliisona <alile24@bergen.org>
2025-04-03 22:26:25 -0400

    random fuzzy noise added

--- cs254/random.c
+++ cs254/random.c
@@ -10,6 +10,8 @@
 // Dynamically calculated size based on the secrets array
 int *queue;
 int queue_size = 0;
+int queue_size_prev = 0;
+int q_updated = 0; 
 float q = 0.1;
 int total_printed = 0;
 
@@ -66,11 +68,16 @@ int black_box_mitigator(int (*target_function)(int),
   int *outputs = malloc(secrets_size * sizeof(int));
 
   for (int i = 0; i < secrets_size; i++) {
+    printf("added to queue (before secret cond.)");
+
     outputs[i] = target_function(secrets[i]);
     pthread_mutex_lock(&queue_mutex);
 
     if (queue_size < secrets_size) {
-      queue[queue_size++] = outputs[i];
+        printf("added to queue");
+        queue_size_prev = queue_size;
+        queue[queue_size++] = outputs[i];
+        q_updated = 0;
     } else {
       for (int j = 1; j < secrets_size; j++) {
         queue[j - 1] = queue[j];
@@ -91,30 +98,15 @@ void *q_interval(void *arg) {
   while (1) {
     pthread_mutex_lock(&queue_mutex);
     // If the queue is empty, double q
-    if (queue_size == 0) {
-      q *= 2;
-      printf("q doubled to %f\n", q);
-      if (q > 16) // Cap q to prevent excessive delay
-        q = 16;
-    } else if (queue_size >= 1) {
-      clock_t current_time = clock();
-      double time_elapsed =
-          (double)(current_time - start_time) / CLOCKS_PER_SEC;
-      int popped = queue[0];
-      for (int i = 1; i < queue_size; i++) {
-        queue[i - 1] = queue[i];
-      }
-      queue_size--;
-      printf("Output: %d\n", popped);
-      printf("Time spent: %f seconds\n", time_elapsed);
-      total_printed++;
 
-      if (queue_size == 0) {
-        q /= 2;
-        printf("q halved to %f\n", q);
-      }
-      start_time = clock();
-    }
+    /*logic here: we want to randomize q everytime there is information to be output. We are not keeping track of a q. 
+    We will show that this randomization method is inefficient, and does not show a logarithmic bound (in actuality, no bound at all)
+   */
+    if (queue_size - queue_size_prev < 0 && !q_updated) { //randomize q if no information output at epoch
+      q =  ((float)rand()/(float)(RAND_MAX)) * 8.0; //get random q (float) between 0 and 8 
+      printf("q time randomized %f\n", q); //print when we have nothing is q, event expected
+      q_updated = 1;
+    } 
 
     pthread_mutex_unlock(&queue_mutex);
 
